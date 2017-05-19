5f4f7fbe407d63d2e856f456ca7be8d57a1fdba0
Alejandro Morente <morente5@hotmail.es>
2017-05-19 11:10:18 +0200

    Phone CTA

--- loop-templates/components/content-phone-cta.php
+++ loop-templates/components/content-phone-cta.php
@@ -6,6 +6,11 @@
  */
 ?>
 <div id="phone-fixed" class=" ">
-    <button type="button" class="btn btn-lg btn-phone btn-warning">Telefonillo</button>
-    <button type="button" class="btn btn-phone-text btn-warning">Telefonillo</button>
+    <?php $contacto_page = get_page_by_title( 'contacto' ); ?>
+    <a href="tel:<?php echo get_field('telefonos', $contacto_page->ID)[0]['telefono']; ?>" class="btn btn-warning btn-block m-t-2 btn-cool">
+<i class="fa fa-phone fa-2"></i>
+<div>
+<small>Si sufres esta patología no dudes en llamarnos</small><?php echo get_field('telefonos', $contacto_page->ID)[0]['telefono']; ?>
+</div>
+</a>
 </div>
