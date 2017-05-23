a1f89dd58c22f6198be63b0e7fca39945d461d09
Alejandro Morente <morente5@hotmail.es>
2017-05-23 10:14:58 +0200

    animate.css

--- loop-templates/components/content-phone-cta.php
+++ loop-templates/components/content-phone-cta.php
@@ -5,12 +5,12 @@
  * @package yofisio
  */
 ?>
-<div id="phone-fixed" class=" ">
+<div id="phone-fixed" class="animated bounceInRight">
     <?php $contacto_page = get_page_by_title( 'contacto' ); ?>
     <a data-rel="external" href="tel:<?php echo get_field('telefonos', $contacto_page->ID)[0]['telefono']; ?>" class="btn btn-warning btn-block m-t-2 btn-cool">
-<i class="fa fa-phone fa-2"></i>
-<div>
-<small>Si sufres esta patología<br>no dudes en llamarnos</small><?php echo get_field('telefonos', $contacto_page->ID)[0]['telefono']; ?>
-</div>
-</a>
+        <i class="fa fa-phone fa-2"></i>
+        <div>
+            <small>Si sufres esta patología<br>no dudes en llamarnos</small><?php echo get_field('telefonos', $contacto_page->ID)[0]['telefono']; ?>
+        </div>
+    </a>
 </div>
