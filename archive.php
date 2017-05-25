fc7081a1f0d17763cf0e368db3d792ac9a66fabd
Alejandro Morente <morente5@hotmail.es>
2017-05-25 20:50:46 +0200

    Archive pages

--- archive.php
+++ archive.php
@@ -6,76 +6,32 @@
  *
  * @package understrap
  */
-
 get_header();
 get_template_part( 'loop-templates/components/content', 'header' );
 get_template_part( 'loop-templates/components/content', 'breadcrumbs' );
+// Only function
+get_template_part( 'loop-templates/components/content', 'main-blog' );
 ?>
 
-<?php
-$container   = get_theme_mod( 'understrap_container_type' );
-$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
-?>
+<section class="wrapper" id="content-wrapper">
 
-<div class="wrapper" id="archive-wrapper">
-
-	<div class="<?php echo esc_html( $container ); ?>" id="content" tabindex="-1">
+	<div class="container" id="content" tabindex="-1">
+		<h2>Últimas entradas sobre <?php echo get_the_category()[0]->name; ?></h2>
 
 		<div class="row">
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check', 'none' ); ?>
-
-			<main class="site-main" id="main">
-
-				<?php if ( have_posts() ) : ?>
-
-					<header class="page-header">
-						<?php
-						//the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="taxonomy-description">', '</div>' );
-						?>
-					</header><!-- .page-header -->
-
-					<?php /* Start the Loop */ ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-
-						<?php
-
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/content', get_post_format() );
-						?>
-
-					<?php endwhile; ?>
-
-				<?php else : ?>
-
-					<?php get_template_part( 'loop-templates/content', 'none' ); ?>
-
-				<?php endif; ?>
-
+			<main class="container container-main" id="main" data-display="5">
+				<?php get_my_posts(get_the_category()[0]->slug, 5, 1); ?>
 			</main><!-- #main -->
 
-			<!-- The pagination component -->
-			<?php understrap_pagination(); ?>
+		</div><!-- .row -->
+		
+		<?php pagination('', 5) ?>
 
-		</div><!-- #primary -->
+	</div><!-- Container end -->
 
-		<!-- Do the right sidebar check -->
-		<?php if ( 'right' === $sidebar_pos || 'both' === $sidebar_pos ) : ?>
 
-			<?php get_sidebar( 'right' ); ?>
+</section><!-- Wrapper end -->
 
-		<?php endif; ?>
-
-	</div> <!-- .row -->
-
-</div><!-- Container end -->
-
-</div><!-- Wrapper end -->
 
 <?php get_footer(); ?>
