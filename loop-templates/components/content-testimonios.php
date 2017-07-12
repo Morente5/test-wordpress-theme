a6fa5ea56ffc1d4e73055b129280760f5ffc5a9c
Alejandro Morente <morente5@hotmail.es>
2017-07-12 22:53:24 +0200

    New styling for testimonios

--- loop-templates/components/content-testimonios.php
+++ loop-templates/components/content-testimonios.php
@@ -13,30 +13,41 @@
   </div>
   
   <div class="container content">
-    <div class="row justify-content-center">
+    <div class="row testimonios no-gutters">
 
       <?php foreach (get_field('testimonios') as $i => $testimonio) { ?>
-        <div class="col-lg-6">
-          <div class="testimonio testimonio-info">
-            <div class="card card-inverse card-info testimonio-section">
+        <div class="col-lg-6 testimonio">
+
+            <div class="card card-inverse card-info">
               <div class="card-block">
-                <?php echo $testimonio['testimonio']; ?>
+                <div class="row">
+                  <div class="col-md-5 testimonio-desc">
+                    <div class="row">
+                      <div class="col-sm-4 col-md-12 testimonio-img">
+                        <?php
+                          $attr = array(
+                            'alt' => $testimonio['imagen']['alt'],
+                          );
+                          echo wp_get_attachment_image( $testimonio['imagen']['id'], 'thumbnail', $attr);
+                        ?>
+                        <div class="testimonio-icon">
+                          <i class="fa fa-thumbs-up" aria-hidden="true"></i>
+                        </div>
+                      </div>
+                      <div class="col-sm-8 col-md-12 testimonio-name">
+                        <p class="testimonio-name-name"><strong><?php echo $testimonio['nombre']; ?></strong></p>
+                        <p class="testimonio-name-designation"><?php echo $testimonio['profesion']; ?></p>
+                        <p class="testimonio-name-treatment"><a href="<?php echo $testimonio['tratamiento_enlace']; ?>"><?php echo $testimonio['tratamiento']; ?></a></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-7 testimonio-text">
+                    <?php echo $testimonio['testimonio']; ?>
+                  </div>
+                </div>
               </div>
             </div>
-            <div class="testimonio-desc">
-                <?php
-                  $attr = array(
-                    'alt' => $testimonio['imagen']['alt'],
-                  );
-                  echo wp_get_attachment_image( $testimonio['imagen']['id'], 'thumbnail', $attr);
-                ?>
-                <div class="testimonio-writer">
-                  <div class="testimonio-writer-name"><?php echo $testimonio['nombre']; ?></div>
-                  <div class="testimonio-writer-designation"><?php echo $testimonio['profesion']; ?></div>
-                  <a href="#" class="testimonio-writer-company"><?php echo $testimonio['tratamiento']; ?></a>
-                </div>
-            </div>
-          </div>
+
         </div>
       <?php } ?>
 
